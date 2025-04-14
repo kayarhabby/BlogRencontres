13726dcb937795628a3bb6edacc77bcd734c6d59
kayarhabby <pasciahershekaya@gmail.com>
2025-04-14 15:26:26 +0200

    update

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,21 @@
+from django.conf import settings
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
 app_name = 'blog'
 urlpatterns = [
     path("", views.index, name="index"),
+    path("liste_blog/", views.liste_blog, name="liste_blog"),
+    path("ajouter/", views.ajouter_blog, name="ajouter_blog"),
+    path("modifier/<int:blog_id>/", views.modifier_blog, name="modifier_blog"),
+    path("supprimer/<int:blog_id>/", views.supprimer_blog, name="supprimer_blog"),
+    path("blog/<int:blog_id>/", views.blog_details, name="blog_details"),
+
 ]
+
+# Ajout de l'accès aux fichiers médias en mode développement
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #urlpatterns = [
 #    path("", views.IndexView.as_view(), name="index"),
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from blog.models import Blog
+
+
 # Create your views here.
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def ajouter_blog(request):
+    return render(request, 'blog/ajouter_blog.html')
+
+def liste_blog(request):
+    liste_blogs = Blog.objects.all()
+    context = {'liste_blogs': liste_blogs}
+    return render(request, 'blog/liste_blog.html', context)
+
+def modifier_blog(request, blog_id):
+    return render(request, 'blog/modifier_blog.html', {'blog_id': blog_id})
+
+def supprimer_blog(request, blog_id):
+    return render(request, 'blog/supprimer_blog.html', {'blog_id': blog_id})
+
+def blog_details(request, blog_id):
+    return render(request, 'blog/blog_details.html', {'blog_id': blog_id})
+
+def commenter_blog(request, blog_id):
+    return render(request, 'blog/commenter_blog.html', {'blog_id': blog_id})
