bc92a63b35ac93235903b8c4c66c07785b66cea1
kayarhabby <pasciahershekaya@gmail.com>
2025-04-14 15:25:26 +0200

    ajout des modeles

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+# create a model for the blog
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="blog/images/", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+# create a model for the comments
+class Comments(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
