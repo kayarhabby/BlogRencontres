b4f9d7dbd6b2096444d80fdbccaaac210ea0bf17
kayarhabby <pasciahershekaya@gmail.com>
2025-04-14 10:34:54 +0200

    creation de l'application django blog

--- BlogRencontres/settings.py
+++ BlogRencontres/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'BlogRencontres.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blogrencontresdb",
+        "USER": "postgres",
+        "PASSWORD": "Pascia1877!",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
