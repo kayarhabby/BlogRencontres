e650e7d667962f289a211728d0cb8690ee9d64f0
kayarhabby <pasciahershekaya@gmail.com>
2025-04-15 11:48:34 +0200

    mise à jour de la page admin, ajout des class admin associé aux models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Blog, Comments, Contact
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'author')
+    list_filter = ('created_at',)
+   # prepopulated_fields = {'title': ('title',)}
+    ordering = ('-created_at',)
+
+admin.site.register(Blog, BlogAdmin)
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('blog', 'name', 'created_at')
+    search_fields = ('blog__title', 'name')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+admin.site.register(Comments, CommentsAdmin)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at')
+    search_fields = ('name', 'email', 'subject')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+admin.site.register(Contact, ContactAdmin)
