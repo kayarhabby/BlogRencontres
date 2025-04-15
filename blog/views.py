bd3345fbff43f53b00d264624609f7e4d3ac64d3
kayarhabby <pasciahershekaya@gmail.com>
2025-04-15 11:49:59 +0200

    suppressio des modèles dont les routes on été supprimés, ajouter_blobs, supprimer, modifier ...

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from blog.models import Blog
-
+from blog.models import Blog, Comments
 
 # Create your views here.
 
 def index(request):
     return render(request, 'blog/index.html')
 
-def ajouter_blog(request):
-    return render(request, 'blog/ajouter_blog.html')
-
 def liste_blog(request):
     liste_blogs = Blog.objects.all()
     context = {'liste_blogs': liste_blogs}
     return render(request, 'blog/liste_blog.html', context)
 
-def modifier_blog(request, blog_id):
-    return render(request, 'blog/modifier_blog.html', {'blog_id': blog_id})
-
-def supprimer_blog(request, blog_id):
-    return render(request, 'blog/supprimer_blog.html', {'blog_id': blog_id})
-
 def blog_details(request, blog_id):
-    return render(request, 'blog/blog_details.html', {'blog_id': blog_id})
+    blog = get_object_or_404(Blog, pk=blog_id)
+    context = {'blog': blog}
+    return render(request, 'blog/blog_details.html', context)
 
 def commenter_blog(request, blog_id):
-    return render(request, 'blog/commenter_blog.html', {'blog_id': blog_id})
+    # obtenir l'ensemble des commentaires pour le blog
+    blog = get_object_or_404(Blog, pk=blog_id)
+    comments = Comments.objects.filter(blog=blog)
+    context = {'blog': blog, 'comments': comments}
+    return render(request, 'blog/commenter_blog.html', context)
