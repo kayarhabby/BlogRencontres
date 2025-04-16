1bb97a1791db72576fa89a8ab3e4a1b076158531
kayarhabby <pasciahershekaya@gmail.com>
2025-04-16 14:35:31 +0200

    update de la vue contact

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
+from blog.forms import ContactForm
 from blog.models import Blog, Comments
-
+from django.templatetags.static import static
 # Create your views here.
 
 def index(request):
@@ -23,3 +24,17 @@ def commenter_blog(request, blog_id):
     comments = Comments.objects.filter(blog=blog)
     context = {'blog': blog, 'comments': comments}
     return render(request, 'blog/commenter_blog.html', context)
+
+def contact(request):
+    context = {
+        'title': 'Contact',
+        'current_page': 'Contact',
+        'banner_image': static('blog/images/my-heart-is-yours.jpg'),
+        'form': ContactForm(request.POST or None)  # Ajout du formulaire ici
+    }
+    # Traitement du formulaire après soumission
+    if request.method == 'POST' and context['form'].is_valid():
+        context['form'].save()
+        return redirect('blog:contact')  # Redirige après soumission réussie
+
+    return render(request, 'blog/contact.html', context)
