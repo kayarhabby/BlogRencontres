653aac606761caa3a3754412c0c629d06c277a51
kayarhabby <pasciahershekaya@gmail.com>
2025-04-16 14:37:37 +0200

    update form avec l'ajout de ContactForm qui utilise les widgets pour personnaliser les champs

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+# forms.py
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your email'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your message', 'rows': 5}),
+        }
